be8505ed4f54ccb6e89d043faa09b5ac94a25444
T. Greg O'Neil <tgregoneil@gmail.com>
2017-02-19 03:20:48 -0600

    replace grep with egrep

--- createPngFileList.sh
+++ createPngFileList.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # createPngFiles.sh
 
-du -a | grep 'png$' | grep -v 'sampleSlide|Homework' | sort | perl -lpe 's/\S+\s+//;s/^/./' > Server/pngFiles
+du -a | grep 'png$' | egrep -v 'sampleSlide|Homework' | sort | perl -lpe 's/\S+\s+//;s/^/./' > Server/pngFiles
