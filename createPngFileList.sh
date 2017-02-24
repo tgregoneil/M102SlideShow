fcb33f03e2e0cee1a8198795943a0d7fc9f38ae5
T. Greg O'Neil <tgregoneil@gmail.com>
2017-02-24 12:35:51 -0600

    removed homework references in pngFiles

--- createPngFileList.sh
+++ createPngFileList.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # createPngFiles.sh
 
-du -a | grep 'png$' | egrep -v 'helpPopupTips|sampleSlide' | perl -lpe 's/\S+\s+//;s/^/./' | sort > Server/pngFiles
+du -a | grep 'png$' | egrep -v 'helpPopupTips|sampleSlide|Homework|FinalExam' | perl -lpe 's/\S+\s+//;s/^/./' | sort > Server/pngFiles
